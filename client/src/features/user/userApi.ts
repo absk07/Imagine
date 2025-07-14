8d018644b35a7c627de0186218b9a9516099280e
absk <noobmaster26128@gmail.com>
2025-07-15 00:44:59 +0530

    finished signin and signup

--- client/src/features/user/userApi.ts
+++ client/src/features/user/userApi.ts
@@ -1,10 +1,19 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import type { RootState } from '../../app/store';
 
-const baseUrl = import.meta.env.VITE_API_URL || 'http://localhost:8080';
+const baseUrl = import.meta.env.VITE_API_URL || 'http://localhost:8080/api/v1';
 
 export const userApi = createApi({
     reducerPath: 'userApi',
-    baseQuery: fetchBaseQuery({ baseUrl: baseUrl }),
+    baseQuery: fetchBaseQuery({ 
+        baseUrl: baseUrl ,
+        prepareHeaders: (headers, { getState }) => {
+            const token = (getState() as RootState).user.token;
+            if (token)
+                headers.set('Authorization', `Bearer ${token}`);
+            return headers;
+        }
+    }),
     endpoints: (build) => ({
         register: build.mutation<any, { username: string; email: string; password: string }>({
             query: (userData) => ({
@@ -19,8 +28,14 @@ export const userApi = createApi({
                 method: 'POST',
                 body: userData,
             }),
+        }),
+        unknowncredit: build.query<any, void>({
+            query: () => ({
+                url: '/user/unknown-credit',
+                method: 'GET'
+            })
         })
     })
 });
 
-export const { useRegisterMutation, useLoginMutation } = userApi;
+export const { useRegisterMutation, useLoginMutation, useUnknowncreditQuery } = userApi;
