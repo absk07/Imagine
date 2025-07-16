b50571efd79e8329897321a000ef66574e515e56
absk <noobmaster26128@gmail.com>
2025-07-17 01:16:23 +0530

    integrated razorpay in backend

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,8 +2,10 @@ import { Router } from 'express';
 const router = Router();
 import userRoutes from './user';
 import iamgeRoutes from './prompt';
+import paymentRoutes from './payment';
 
 router.use('/user', userRoutes);
 router.use('/text-to-image', iamgeRoutes);
+router.use('/buy-uc', paymentRoutes);
 
 export default router;
